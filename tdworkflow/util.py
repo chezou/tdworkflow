4fce11e851202867b12271e19ebaa79657c036be
Aki Ariga <ariga@treasure-data.com>
2019-12-06 22:34:23 +0900

    fix: ensure util.archive_file to create file in the root directory

--- tdworkflow/util.py
+++ tdworkflow/util.py
@@ -1,3 +1,4 @@
+import glob
 import io
 import logging
 import os
@@ -26,7 +27,11 @@ def archive_files(target_dir: str, exclude_patterns: List[str]) -> io.BytesIO:
 
     _bytes = io.BytesIO()
     with tarfile.open(mode="w:gz", fileobj=_bytes) as tar:
-        tar.add(target_dir, arcname="./", filter=exclude_files(pattern))
+        for fn in glob.glob(os.path.join(target_dir, "**"), recursive=True):
+            if not re.search(pattern, fn) and os.path.basename(fn) != "":
+                write_fn = fn.replace(f"{target_dir}/", "")
+                logger.info(f"Added {fn} as {write_fn}")
+                tar.add(fn, write_fn, recursive=False)
 
     _bytes.seek(0)
     return _bytes
--- tests/test_util.py
+++ tests/test_util.py
@@ -4,10 +4,10 @@ from tdworkflow.util import archive_files
 
 
 def test_archive_files():
-    expected_files = [".", "./py_scripts", "./py_scripts/exec.py", "./main.dig"]
+    expected_files = ["py_scripts", "py_scripts/exec.py", "main.dig"]
 
     target_dir = "tests/resources/sample_project"
-    excludes = ["ignore_dir"]
+    excludes = ["ignore_dir", "__ignoredir__"]
     data = archive_files(target_dir, excludes)
 
     files = []
